7ec2a8cc34ded9e7943d964dec32bd57309a4927
Overflow922 <genius922@yandex.ru>
2023-06-28 19:59:13 +0300

    Форма антропометрии

---
+++ front/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+/* todo: We will have dozens of rest endpoints. We should generalize end point stubs
+e.g. read jsons from stubs folder
+ */
+
+app.use('/anthro', (req, res) => {
+    res.send({
+        gender: 'мужской',
+        age: '42',
+        height: '160',
+        weight: '82',
+    })
+})
+
+app.listen(8080, () => console.log('REST API is running on http://localhost:8080'));
