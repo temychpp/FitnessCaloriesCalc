4bf365380acd0878f90a955fce4b3c86d990dad4
Yury <genius922@yandex.ru>
2023-07-07 19:48:25 +0300

    Запросы для вкладки расчетов (#19)

--- front/server.js
+++ front/server.js
@@ -21,4 +21,25 @@ app.use('/500/person/anthro', (req, res) => {
     res.status(500).send()
 })
 
+// ======= CALC ========
+app.use('/calc/body_mass_index', (req, res) => {
+    res.send("1")
+})
+
+app.use('/calc/calories_mifflin_stjeor', (req, res) => {
+    res.send("2")
+})
+
+app.use('/calc/ideal_weight_lorenz', (req, res) => {
+    res.send("3")
+})
+
+app.use('/calc/ideal_weight_devine', (req, res) => {
+    res.send("4")
+})
+
+app.use('/calc/ideal_weight_broca', (req, res) => {
+    res.send("5")
+})
+
 app.listen(8080, () => console.log('REST API is running on http://localhost:8080'));
