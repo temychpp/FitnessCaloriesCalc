423e6c22689db8ccf99a2a9390a0d1b80923a86f
Overflow922 <genius922@yandex.ru>
2023-07-07 18:55:21 +0300

    Запросы для вкладки расчетов

--- front/src/components/Calc.js
+++ front/src/components/Calc.js
@@ -7,74 +7,55 @@ const emitLoadingError = (message) => {
     emitCustomEvent('error-event', message);
 }
 
-async function getAllCalculations(id) {
+const GET_OPTIONS = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
 
+let results = {
+    bmi: '',
+    msj: '',
+    lorenz: '',
+    devine: '',
+    broca: ''
+}
+
+async function getAllCalculations(id) {
+    await Promise.all([
+        getBmi(id).then(data => results.bmi = data),
+        getCaloriesMSJ(id).then(data => results.msj = data),
+        getIdealWeightLorenz(id).then(data => results.lorenz = data),
+        getIdealWeightDevine(id).then(data => results.devine = data),
+        getIdealWeightBroca(id).then(data => results.broca = data)
+    ])
 }
 
 
 async function getBmi(userId) {
-    emitCustomEvent('start-loading');
-    return fetchWithTimeout('http://localhost:8080/calc/body_mass_index?id=' + userId, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(data => data.json())
-        .finally(_ => {
-            emitCustomEvent('loaded');
-        })
+    return fetchWithTimeout('http://localhost:8080/calc/body_mass_index?id=' + userId, GET_OPTIONS)
+        .then(data => data.json())
 }
 
 async function getCaloriesMSJ(id) {
-    emitCustomEvent('start-loading');
-    return fetchWithTimeout('http://localhost:8080/calc/calories_mifflin_stjeor?id=', +id, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(data => data.json())
-        .finally(_ => {
-            emitCustomEvent('loaded');
-        })
+    return fetchWithTimeout('http://localhost:8080/calc/calories_mifflin_stjeor?id=' + id, GET_OPTIONS)
+        .then(data => data.json())
 }
 
 async function getIdealWeightLorenz(id) {
-    emitCustomEvent('start-loading');
-    return fetchWithTimeout('http://localhost:8080/calc/ideal_weight_lorenz?id=', +id, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(data => data.json())
-        .finally(_ => {
-            emitCustomEvent('loaded');
-        })
+    return fetchWithTimeout('http://localhost:8080/calc/ideal_weight_lorenz?id=' + id, GET_OPTIONS)
+        .then(data => data.json())
 }
 
 async function getIdealWeightDevine(id) {
-    emitCustomEvent('start-loading');
-    return fetchWithTimeout('http://localhost:8080/calc/ideal_weight_devine?id=', +id, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(data => data.json())
-        .finally(_ => {
-            emitCustomEvent('loaded');
-        })
+    return fetchWithTimeout('http://localhost:8080/calc/ideal_weight_devine?id=' + id, GET_OPTIONS)
+        .then(data => data.json())
 }
 
 async function getIdealWeightBroca(id) {
-    emitCustomEvent('start-loading');
-    return fetchWithTimeout('http://localhost:8080/calc/ideal_weight_broca?id=', +id, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(data => data.json())
-        .finally(_ => {
-            emitCustomEvent('loaded');
-        })
+    return fetchWithTimeout('http://localhost:8080/calc/ideal_weight_broca?id=' + id, GET_OPTIONS)
+        .then(data => data.json())
 }
 
 export default function Calc() {
@@ -82,61 +63,58 @@ export default function Calc() {
     const [form] = Form.useForm()
 
     useEffect(() => {
-
-        getAllCalculations(1).then(result => {
-            form.setFieldValue('bodyMassIndex',result.bmi)
-            form.setFieldValue('idealWeightBroca', result.idealWeightBroca)
-            form.setFieldValue('idealWeightLorenz', result.idealWeightLorenz)
-            form.setFieldValue('idealWeightDevine', result.idealWeightDevine)
-            form.setFieldValue('сaloriesMSJ', result.caloriesMSJ)
-
-        })
+        emitCustomEvent('start-loading');
+        getAllCalculations(1).then(_ => {
+            form.setFieldValue('bodyMassIndex', results.bmi)
+            form.setFieldValue('idealWeightBroca', results.msj)
+            form.setFieldValue('idealWeightLorenz', results.lorenz)
+            form.setFieldValue('idealWeightDevine', results.devine)
+            form.setFieldValue('сaloriesMSJ', results.broca)
+        }).catch(_ => emitLoadingError('Ошибка загрузки расчетов'))
+            .finally(_ => {
+                emitCustomEvent('loaded');
+            })
     }, []);
 
-
-    const onFinish = (values) => {
-        getAllCalculations(1)
-    }
 
     return (<>
         <h1>Расчеты</h1>
         <Form
             form={form}
-            name="calc"
-            onFinish={onFinish}>
+            name="calc">
             <Form.Item
                 name="bodyMassIndex"
                 label="Индекс массы тела(1835)"
             >
-                <Input/>
+                <Input disabled={true}/>
             </Form.Item>
 
             <Form.Item
                 name="idealWeightBroca"
                 label="Вес по Брока(1871)"
             >
-                <Input/>
+                <Input disabled={true}/>
             </Form.Item>
 
             <Form.Item
                 name="idealWeightLorenz"
                 label="Вес по Лоренцу(1929)"
             >
-                <Input/>
+                <Input disabled={true}/>
             </Form.Item>
 
             <Form.Item
                 name="idealWeightDevine"
                 label="Вес по Девайну(1974)"
             >
-                <Input/>
+                <Input disabled={true}/>
             </Form.Item>
 
             <Form.Item
                 name="сaloriesMSJ"
                 label="Калории по Миффлин—Сан-Жеора(1990)"
             >
-                <Input/>
+                <Input disabled={true}/>
             </Form.Item>
 
         </Form>
